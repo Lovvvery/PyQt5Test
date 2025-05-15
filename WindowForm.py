495a26c1f8a5e5c3e33fd024385d341b02a2bdae
loveryLove <itsdeslon@gmail.com>
2025-05-15 22:43:37 +0700

    Измениола расположение элемента label

--- WindowForm.py
+++ WindowForm.py
@@ -9,26 +9,21 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        # Заголовок и минимальные размеры окна
         self.setWindowTitle("Мое первое PyQt5 приложение")
         self.setMinimumSize(400, 200)
 
-        # Центральный виджет и основной layout
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
 
-        # Основной вертикальный layout
         main_layout = QVBoxLayout()
 
-        # Метка
+        input_layout = QHBoxLayout()
         self.label = QLabel("Введите ваше имя:")
-        main_layout.addWidget(self.label)
-
-        # Поле ввода
         self.text_input = QLineEdit()
-        main_layout.addWidget(self.text_input)
+        input_layout.addWidget(self.label)
+        input_layout.addWidget(self.text_input)
+        main_layout.addLayout(input_layout)
 
-        # Горизонтальный layout для кнопки
         button_layout = QHBoxLayout()
         self.button = QPushButton("Поздороваться")
         self.button.clicked.connect(self.say_hello)
@@ -36,7 +31,6 @@ class MainWindow(QMainWindow):
         button_layout.addWidget(self.button)
         main_layout.addLayout(button_layout)
 
-        # Устанавливаем layout на центральный виджет
         central_widget.setLayout(main_layout)
 
     def say_hello(self):
